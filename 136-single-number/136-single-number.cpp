8f56be82fc7a7241ad93a9689e2bfa52206d468a
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-01-17 19:09:46 +0530

    Time: 45 ms (10.83%), Space: 20.1 MB (11.19%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp[nums[i]] == 1)
+                return nums[i];
+        }
+        return -1;
+        /*
         int res = 0;
         for(int i = 0; i < nums.size(); i++) {
             res ^= nums[i];
         }
-        return res;
+        return res;*/
     }
 };
