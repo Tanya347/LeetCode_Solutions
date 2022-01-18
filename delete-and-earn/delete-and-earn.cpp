b2d7990ff3d6b98d7a8c326fddfc7d001267263b
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-01-18 20:27:29 +0530

    Time: 6 ms, Memory: 9.8 MB - LeetHub

--- delete-and-earn/delete-and-earn.cpp
+++ delete-and-earn/delete-and-earn.cpp
@@ -6,7 +6,7 @@ public:
             return nums[0];
         int max_point = 0;
         
-        vector<int> map(10001, 0);
+        unordered_map<int, int> map;
         
         for(int i = 0; i < n; i++) {
             map[nums[i]]++;
