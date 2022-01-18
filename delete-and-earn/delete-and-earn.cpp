1df7fc7f9952d232216836538a821f120943f769
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-01-18 20:26:34 +0530

    Time: 11 ms, Memory: 11.5 MB - LeetHub

--- delete-and-earn/delete-and-earn.cpp
+++ delete-and-earn/delete-and-earn.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
-    
-    int maxPoints(vector<int> map, int point, vector<int>& dp) {
-           
-        if(point <= 0)
-            return 0;
-        
-        if(dp[point] != -1) {
-            return dp[point];
-        }
-        //delete the current element
-        int points1 = map[point]*point;
-        points1 += maxPoints(map, point - 2, dp);
-        
-        //don't delete the element
-        int points2 = maxPoints(map, point - 1, dp);
-        
-        dp[point] = max(points1, points2);
-        
-        return dp[point];
-    }
-    
     int deleteAndEarn(vector<int>& nums) {
-        
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-        
-        if(n == 1) { 
+        if(n == 1)
             return nums[0];
-        }
+        int max_point = 0;
         
-        //map the count of every element
-        //calculate max value in array
+        vector<int> map(10001, 0);
         
-        vector<int> map(10001,0);
-        int max_points = 0;
         for(int i = 0; i < n; i++) {
             map[nums[i]]++;
-            max_points = max(nums[i], max_points);
+            max_point = max(max_point, nums[i]);
         }
         
-        vector<int> dp(max_points + 1, -1);
+        vector<int> dp(max_point + 1);
         dp[0] = 0;
+        dp[1] = map[1];
+        int ans = map[1];
         
-        return maxPoints(map, max_points, dp);
+        for(int i = 2; i <= max_point; i++) {
+            dp[i] = max(dp[i - 1], map[i]*i + dp[i - 2]);
+            ans = dp[i];
+        }
+        
+        return ans;
     }
 };
