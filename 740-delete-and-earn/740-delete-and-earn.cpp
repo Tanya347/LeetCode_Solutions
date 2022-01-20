d2145c1c34fadc408e179ddc3cd84ddb99bc30a0
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-01-20 22:33:57 +0530

    Time: 8 ms (69.42%), Space: 9.9 MB (58.94%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)
+            return nums[0];
+        int max_point = 0;
+        
+        unordered_map<int, int> map;
+        
+        for(int i = 0; i < n; i++) {
+            map[nums[i]]++;
+            max_point = max(max_point, nums[i]);
+        }
+        
+        vector<int> dp(max_point + 1);
+        dp[0] = 0;
+        dp[1] = map[1];
+        int ans = map[1];
+        
+        for(int i = 2; i <= max_point; i++) {
+            dp[i] = max(dp[i - 1], map[i]*i + dp[i - 2]);
+            ans = dp[i];
+        }
+        
+        return ans;
+    }
+};
