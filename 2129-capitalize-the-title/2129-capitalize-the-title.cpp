d50cb23c3fa62ff25aa56eefda71d0b23d94299b
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-01-24 09:29:48 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (35.48%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -4,81 +4,62 @@ public:
     
     string capitalizeTitle(string title) {
         
-        transform(title.begin(), title.end(), title.begin(), ::tolower);
 
+        int wordlength = 0;
+    int i = 0, j = -1;
     title = title + " ";
-    string word = "";
-    string ans = "";
 
-    for (int i = 0; i < title.length(); i++)
+    while (i < title.length())
     {
+
+        // if there is no space
+
         if (title[i] != ' ')
-            word = word + title[i];
+        {
+            if (j == -1)
+                j = i;
+            wordlength++;
+            i++;
+        }
+
+        // if we encounter a space
+
         else
         {
-            if (word.length() < 3 && ans.length() != 0)
-                ans = ans + " " + word;
+            // if word length is 1 or 2 change all to lowercase
 
-            else if (word.length() < 3 && ans.length() == 0)
-                ans = word;
+            if (wordlength == 1 || wordlength == 2)
+            {
+                while (wordlength >= 0)
+                {
+                    title[j] = tolower(title[j]);
+                    j++;
+                    wordlength--;
+                }
+            }
+
+            // else change first letter to upper case rest to lowercase
 
             else
             {
-                word[0] = toupper(word[0]);
-                if (ans.length() != 0)
-                    ans = ans + " " + word;
-                else
-                    ans = word;
+                title[j] = toupper(title[j]);
+
+                j++;
+                while (wordlength >= 0)
+                {
+                    title[j] = tolower(title[j]);
+                    j++;
+                    wordlength--;
+                }
             }
-            word = "";
+
+            // reinitialise everything for next word
+            j = -1;
+            wordlength = 0;
+            i++;
         }
     }
-    return ans;
-        /*
-        int wordlength = 0;
-        int i = 0, j = -1;
-        
-        while(i < title.length()) {
-            
-            //if there is no space
-            
-            if(title[i] != ' ' && title[i] != '\0') {
-                if(j == -1)
-                    j = i;
-                wordlength++;
-                i++;
-            }
-            
-            //if we encounter a space
-            
-            else if(title[i] == ' ') {
-                
-                //if word length is 1 or 2 change all to lowercase
-                
-                if(wordlength == 1 || wordlength == 2) {
-                    while(j < wordlength) {
-                        title[i] = tolower(title[i]);
-                        j++;
-                    }
-                }
-                
-                //else change first letter to upper case rest to lowercase
-                
-                else {
-                    title[j] = toupper(title[i]);
-                    j++;
-                    while(j < wordlength) {
-                        title[i] = tolower(title[i]);
-                        j++;
-                    }
-                }
-                
-                //reinitialise everything for next word
-                j = -1;
-                wordlength = 0;
-                i++;
-            }
-        }
-        return title;*/
+    title.pop_back();
+    return title;
     }
 };
