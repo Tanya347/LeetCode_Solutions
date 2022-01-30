7eeec7f700f7e306657765025bad979f48a5a764
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-01-30 21:41:37 +0530

    Time: 125 ms (42.96%), Space: 58.9 MB (80.46%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        
+        //two pointers that will travel from beginning and end
         int l = 0, r = height.size() - 1;
+        //global maximum area
         int maxArea = INT_MIN;
+        
         while(l < r) {
+            
             int currArea = min(height[l], height[r]) * (r - l);
-            if(maxArea < currArea)
-                maxArea = currArea;
+            
+            maxArea = max(maxArea, currArea);
+            
+            //moving to the greater height in order to attain the max area
             if(height[l] > height[r])
                 r--;
             else
                 l++;
         }
+        
         return maxArea;
     }
 };
