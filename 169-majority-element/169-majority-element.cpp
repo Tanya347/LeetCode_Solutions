491cc978e3872c8f341780535576552368d255d2
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-01-31 14:33:43 +0530

    Time: 24 ms (50.64%), Space: 19.6 MB (51.02%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+        unordered_map<int, int> mp;
         
-        //As we know that the majority element is present in more than n/2 times
-        //it is guaranteed that whenever we sort the array the middle element will be 
-        //the majority element
-        return nums[n/2];
+        for(int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+            if(mp[nums[i]] > nums.size()/2)
+                return nums[i];
+        }
+        return -1;
     }
 };
