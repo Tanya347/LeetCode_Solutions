2bd28126547fcb78e3f5782eac6792ea78eb9d12
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-01-31 09:59:50 +0530

    Time: 4 ms (50.86%), Space: 7.2 MB (33.27%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        //rotate leftwards
+        int n = matrix.size();
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        //swap columns
+        for(int i = 0; i < n; i++) {
+            int s = 0, e = n - 1;
+            while(s < e) {
+                int temp = matrix[i][s];
+                matrix[i][s] = matrix[i][e];
+                matrix[i][e] = temp;
+                s++;
+                e--;
+            }
+        }
+    }
+};
