503349974425c56334e9050afabb7993002517a0
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-01 14:23:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (22.67%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
@@ -2,11 +2,16 @@ class Solution {
 public:
     bool hasAlternatingBits(int n) {
         
+        //will retrieve the last bit of the integer
         int binary = n & 1; 
+        
+        //will shift the bits towards right by 1 and remove last bit
         n = n >> 1;
-        bool isAlternate = true;
         
         while(n != 0) {
+            
+            //if the consecutive numbers are not equal i.e they are alternate then shift
+            //else return false
             if(binary != (n & 1)) {
                 binary = n & 1;
                 n = n >> 1;
@@ -15,6 +20,6 @@ public:
                 return false;
         }
         
-        return isAlternate;
+        return true;
     }
 };
