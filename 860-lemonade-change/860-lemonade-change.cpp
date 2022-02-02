db4fbac51bbd1fcb9f850c7c2d50e0481e9e2f47
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-02 09:09:03 +0530

    Time: 100 ms (71.51%), Space: 83.2 MB (96.37%) - LeetHub

---
+++ 860-lemonade-change/860-lemonade-change.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+    
+        int Count5 = 0, Count10 = 0, Count20 = 0; 
+        
+        for(int i = 0; i < bills.size(); i++) {
+        
+            if(bills[i] == 5)
+                Count5++;
+        
+            else if(bills[i] == 10) {
+                
+                if(Count5 > 0)
+                    Count5--;
+                
+                else return false;
+                
+                Count10++;
+            }
+        
+            else if(bills[i] == 20) {
+                
+                if(Count5 > 0 && Count10 > 0) {
+                    Count5--;
+                    Count10--;
+                }
+                
+                else if(Count5 > 2)
+                    Count5 -= 3;
+                
+                else return false;
+                
+                Count20++;
+            }
+        }
+    
+        return true;
+    }
+};
