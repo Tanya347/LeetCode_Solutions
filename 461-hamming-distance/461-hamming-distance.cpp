54ba54796463ca3037661466d6c848e117764fd8
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-02 09:28:39 +0530

    Time: 4 ms (13.61%), Space: 5.9 MB (25.45%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.cpp
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -2,23 +2,13 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         
-        int x_bit, y_bit, count = 0;
+        x = x ^ y;
+        int count = 0;
         
-        while(x != 0 || y != 0) {
-            
-            x_bit = x & 1;
-            x = x >> 1;
-            
-            y_bit = y & 1;
-            y = y >> 1;
-            
-            if(x_bit != y_bit)
+        while(x != 0) {
+            if(x & 1 == 1)
                 count++;
-            
-            //or we can also do this
-            
-//             if(x_bit ^ y_bit == 1)
-//                 count++;
+            x = x >> 1;
         }
         
         return count;
