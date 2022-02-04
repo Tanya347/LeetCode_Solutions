2d3b1782cbdcfb09567d2dc57d9d76fb9dd85855
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-04 20:12:33 +0530

    Time: 249 ms (18.24%), Space: 83.9 MB (55.07%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -4,15 +4,15 @@ public:
         
         int maxLength = 0, sum = 0;
         int n = nums.size();
-        vector<int> index(2 * n + 1, -2);
-        index[n] = -1;
+        unordered_map<int, int> index;
+        index[0] = -1;
         
         for(int i = 0; i < n; i++) {
             sum += (nums[i]) ? 1 : -1;
-            if(index[sum + n] >= -1)
-                maxLength = max(maxLength, i - index[sum + n]);
+            if(index.count(sum))
+                maxLength = max(maxLength, i - index[sum]);
             else
-                index[sum + n] = i;
+                index[sum] = i;
         }
         
         return maxLength;
