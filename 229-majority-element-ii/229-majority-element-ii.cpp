481d0cdbdbc868c62dbeb4c030bf3e81ccacb984
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-06 16:11:35 +0530

    Time: 20 ms (40.08%), Space: 15.9 MB (13.01%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -2,53 +2,26 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-        if(nums.size() == 1)
-            return nums;
-        
-        int count1 = 0, count2 = 0, majority1 = 0, majority2 = 0;
-        
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == majority1)
-                count1++;
-            
-            else if(nums[i] == majority2)
-                count2++;
-            
-            else if(count1 == 0) {
-                majority1 = nums[i];
-                count1 = 1;
-            }
-            
-            else if(count2 == 0) {
-                majority2 = nums[i];
-                count2 = 1;
-            }
-            
-            else {
-                count1--;
-                count2--;
-            }
-            
-        }
-        
-        count1 = 0, count2 = 0;
-        
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == majority1)
-                count1++;
-            else if(nums[i] == majority2)
-                count2++;
-        }
-        
+        sort(nums.begin(), nums.end());
         vector<int> ans;
         
-        if(count1 > nums.size() / 3) 
-            ans.push_back(majority1);
+        int n = nums.size() / 3, count = 1;
         
-        if(count2 > nums.size() / 3) 
-            ans.push_back(majority2);
+        for(int i = 0; i < nums.size(); i++) {
+            
+            if(i >= 1 && nums[i] == nums[i - 1])
+                count++;
+            else if(i >= 1 && nums[i] != nums[i - 1])
+                count = 1;
+            
+            if(count > n) {
+                if(ans.empty() || (ans.size() > 0 && ans[ans.size() - 1] != nums[i]))
+                    ans.push_back(nums[i]);
+                count = 1;
+            }
+            
+        }
         
         return ans;
     }
 };
-//8 8 8 8
