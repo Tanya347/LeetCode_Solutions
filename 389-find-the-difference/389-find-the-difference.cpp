4ee9a6484c482b49060bbd99c3633665a70174d7
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-07 17:56:57 +0530

    Time: 12 ms (6.98%), Space: 6.5 MB (87.58%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        
-        if(s.length() == 0)
-            return t[0];
-        
-        unordered_map<char, int> hash1;
-        unordered_map<char, int> hash2;
-        
-        for(int i = 0; i < s.length(); i++) {
-            hash1[s[i]]++;
-        }
-        
+        int count = 0;
         for(int i = 0; i < t.length(); i++) {
-            hash2[t[i]]++;
-            if(hash1.find(t[i]) == hash1.end() || hash1[t[i]] < hash2[t[i]])
-                return t[i];
+            count += t[i];
         }
         
-        return ' ';
+        for(int j = 0; j < s.length(); j++) {
+            count -= s[j];
+        }
+        
+        char ans = count;
+        return ans;
     }
 };
