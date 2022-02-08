cb41ce7917a33b75a0f3c05a9ced2873b59460fd
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-08 07:04:50 +0530

    Time: 3 ms (53.62%), Space: 5.8 MB (95.57%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int addDigits(int num) {
         
-        int ans = 0;
-        
-        while(num > 0) {
+        while(num > 9) {
             
-            ans += num % 10;
-            num = num / 10;
-            
-            if(num == 0 && ans > 9) {
-                num = ans;
-                ans = 0;
+            int sum = 0;
+            while(num != 0) {
+                sum += num % 10;
+                num = num / 10;
             }
+            num = sum;
         }
-        return ans;
+        return num;
     }
 };
