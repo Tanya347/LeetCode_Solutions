c6751fc2491551e011e47aa144dd206a9cf6f837
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-13 12:06:43 +0530

    Time: 4 ms (57.43%), Space: 7.6 MB (42.48%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsets_helper(vector<int>& nums, int n) {
+        
+        if(n == 0)
+            return {{}};
+        
+        vector<vector<int>> ans = subsets_helper(nums, n - 1);
+        vector<vector<int>> temp = ans;
+        
+        for(int i = 0; i < temp.size(); i++) {
+            temp[i].push_back(nums[n - 1]);
+        }
+        
+        ans.insert(ans.end(), temp.begin(), temp.end());
+        return ans;
+        
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        return subsets_helper(nums, nums.size());
+    }
+};
