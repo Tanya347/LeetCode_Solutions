d876d44b27d7ccf29b362dc1f83b29afaf72f69e
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-23 10:41:29 +0530

    Time: 7 ms (6.17%), Space: 5.9 MB (71.23%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        
         if(m == 1 || n == 1)
             return 1;
         
         int dp[m][n];
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(i == 0 || j == 0)
+        
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                if(i == m - 1 || j == n - 1)
                     dp[i][j] = 1;
                 else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
                 }
             }
         }
-        return dp[m - 1][n - 1];
+        
+        return dp[0][0];
     }
 };
