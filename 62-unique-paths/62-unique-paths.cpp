059f72772830d988d0053c425a343f5161c2ea32
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-23 18:07:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (71.23%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -5,18 +5,17 @@ public:
         if(m == 1 || n == 1)
             return 1;
         
-        int dp[m][n];
+        int dp[n];
+        for(int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
         
-        for(int i = m - 1; i >= 0; i--) {
-            for(int j = n - 1; j >= 0; j--) {
-                if(i == m - 1 || j == n - 1)
-                    dp[i][j] = 1;
-                else {
-                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
-                }
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                    dp[j] += dp[j - 1];
             }
         }
         
-        return dp[0][0];
+        return dp[n - 1];
     }
 };
