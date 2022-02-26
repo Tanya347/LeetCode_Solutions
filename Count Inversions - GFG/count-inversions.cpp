354f2621408169c1a7c9c5fe53202870927f30d6
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-26 15:51:10 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -5,59 +5,52 @@ using namespace std;
 
  // } Driver Code Ends
 class Solution{
+  public:
     // arr[]: Input Array
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
- private:
-  long long merge(long long *arr, long long *temp, long long l, long long mid, long long r){
-      long long i,j,k;
-      long long inv_cnt = 0;
-      
-      i = l;
-      j = mid;
-      k = l;
-      
-      while((i <= mid - 1) && (j<=r)){
-          if(arr[i] <= arr[j]){
-            temp[k++] = arr[i++];}
-          else{
-              temp[k++] = arr[j++];
-              
-              inv_cnt += (mid - i);
-          }
-      }
-      
-      while(i<=mid-1){
-        temp[k++] = arr[i++];}
-        
-      while(j<=r){
-        temp[k++] = arr[j++];}
+    long long count = 0;
     
-        for(int i = l; i<=r;i++){
-            arr[i] = temp[i];}
+    void merge(long long *arr, long long *sub, long long s, long long mid, long long e) {
+        long long i = s, j = mid + 1, k = s;
         
-        return inv_cnt;
-      
-  }
-  long long mergeSort(long long *arr, long long *temp, long long l, long long r){
-      long long mid;
-      long long inv_cnt = 0;
-      
-      if(r>l){
-          mid = (l+r)/2;
-          inv_cnt += mergeSort(arr,temp,l, mid);
-          inv_cnt += mergeSort(arr,temp,mid+1,r);
-          
-          inv_cnt += merge(arr,temp,l,mid+1,r);
-      }
-      return inv_cnt;
-  }
-  public:
+        while((i <= mid) && (j <= e)) {
+            if(arr[i] <= arr[j])
+                sub[k++] = arr[i++];
+            else {
+                sub[k++] = arr[j++];
+                count += (mid - i + 1);
+            }
+        }
+        
+        while(i <= mid)
+            sub[k++] = arr[i++];
+            
+        while(j <= e)
+            sub[k++] = arr[j++];
+            
+        for(int i = s; i <= e; i++)
+            arr[i] = sub[i];
+            
+    }
+    
+    void mergeSort(long long arr[], long long sub[], long long s, long long e) {
+        if(s >= e)
+            return;
+            
+        long long mid = (s + e)/2;
+        mergeSort(arr, sub, s, mid);
+        mergeSort(arr, sub, mid + 1, e);
+        
+        merge(arr, sub, s, mid, e);
+    }
+    
     long long int inversionCount(long long arr[], long long N)
     {
-        long long temp[N];
-        long long ans = mergeSort(arr,temp,0,N-1);
-        return ans;
+        // Your Code Here
+        long long subarray[N];
+        mergeSort(arr, subarray, 0, N - 1);
+        return count;
     }
 
 };
