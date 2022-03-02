ac431789496b22496a7ca80d71a20d3bbfc7d53c
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-03-02 10:39:34 +0530

    Time: 96 ms (5.04%), Space: 142.2 MB (5.07%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,17 +1,57 @@
-class Solution {
+
+    class Solution {
+    int helper(string s, string t, int i, int j, vector<vector<int>> &dp) {
+        
+        if(s.empty() || t.empty())
+            return 0;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        if(s[0] == t[0])
+            dp[i][j] = 1 + helper(s.substr(1), t.substr(1), i + 1, j + 1, dp);
+            
+        else
+            dp[i][j] = helper(s, t.substr(1), i, j + 1, dp);
+        
+        return dp[i][j];
+    }
+ 
 public:
     bool isSubsequence(string s, string t) {
         
-        if(s.length() == 0)
-            return true;
-        
-        
-        if(t.length() == 0)
+        if (s.size() > t.size()) 
             return false;
         
-        if(s[0] == t[0])
-            return isSubsequence(s.substr(1), t.substr(1));
+        vector<vector<int>> dp(s.size(),vector<int> (t.size(),-1));
         
-        return isSubsequence(s, t.substr(1));
+        return helper(s, t, 0, 0,dp) == s.size();
     }
 };
+
+// int helper(string s, string t, int i, int j, vector<vector<int>> dp) {
+        
+//         if(s.empty() || t.empty())
+//             return 0;
+        
+//         if(dp[i][j] != -1)
+//             return dp[i][j];
+        
+//         if(s[0] == t[0])
+//             dp[i][j] = 1 + helper(s.substr(1), t.substr(1), i + 1, j + 1, dp);
+            
+//         else
+//             dp[i][j] = helper(s, t.substr(1), i, j + 1, dp);
+        
+//         return dp[i][j];
+//     }
+    
+//     bool isSubsequence(string s, string t) {
+        
+//         if(s.length() > t.length())
+//             return false;
+        
+//         vector<vector<int>> dp(s.length(), vector<int>(t.length(), -1));
+//         return helper(s, t, 0, 0, dp) == s.length();
+//     }
+// };
