d2499e10d703f2f7a65cc3dabbce853b16bb2574
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-03-02 11:07:09 +0530

    Time: 151 ms (5.04%), Space: 288 MB (5.07%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -2,17 +2,17 @@
     class Solution {
     int helper(string s, string t, int i, int j, vector<vector<int>> &dp) {
         
-        if(s.empty() || t.empty())
+        if(i <= 0 || j <= 0)
             return 0;
         
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        if(s[0] == t[0])
-            dp[i][j] = 1 + helper(s.substr(1), t.substr(1), i + 1, j + 1, dp);
+        if(s[i - 1] == t[j - 1])
+            dp[i][j] = 1 + helper(s, t, i - 1, j - 1, dp);
             
         else
-            dp[i][j] = helper(s, t.substr(1), i, j + 1, dp);
+            dp[i][j] = helper(s, t, i, j - 1, dp);
         
         return dp[i][j];
     }
@@ -20,38 +20,16 @@
 public:
     bool isSubsequence(string s, string t) {
         
-        if (s.size() > t.size()) 
+        int a = s.length(), b = t.length();
+        
+        if (a > b) 
             return false;
         
-        vector<vector<int>> dp(s.size(),vector<int> (t.size(),-1));
+        if(a == 0)
+            return true;
         
-        return helper(s, t, 0, 0,dp) == s.size();
+        vector<vector<int>> dp(a + 1,vector<int> (b + 1,-1));
+        
+        return helper(s, t, a, b, dp) == a;
     }
 };
-
-// int helper(string s, string t, int i, int j, vector<vector<int>> dp) {
-        
-//         if(s.empty() || t.empty())
-//             return 0;
-        
-//         if(dp[i][j] != -1)
-//             return dp[i][j];
-        
-//         if(s[0] == t[0])
-//             dp[i][j] = 1 + helper(s.substr(1), t.substr(1), i + 1, j + 1, dp);
-            
-//         else
-//             dp[i][j] = helper(s, t.substr(1), i, j + 1, dp);
-        
-//         return dp[i][j];
-//     }
-    
-//     bool isSubsequence(string s, string t) {
-        
-//         if(s.length() > t.length())
-//             return false;
-        
-//         vector<vector<int>> dp(s.length(), vector<int>(t.length(), -1));
-//         return helper(s, t, 0, 0, dp) == s.length();
-//     }
-// };
