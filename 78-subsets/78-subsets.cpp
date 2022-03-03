916436e53e6bde4b2c934d3d58f835986858b064
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-03-03 18:33:44 +0530

    Time: 4 ms (54.20%), Space: 7.8 MB (36.17%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> subsets_helper(vector<int>& nums, int n) {
-        
-        if(n == 0)
-            return {{}};
-        
-        vector<vector<int>> ans = subsets_helper(nums, n - 1);
-        vector<vector<int>> temp = ans;
-        
-        for(int i = 0; i < temp.size(); i++) {
-            temp[i].push_back(nums[n - 1]);
+    void subset_helper(vector<int> nums, vector<vector<int>>& ans, vector<int>& temp, int i, int k) {
+        //if size of subset array becomes equal to the length of required subset
+        if(temp.size() == k) {
+            ans.push_back(temp);
+            return;
         }
-        
-        ans.insert(ans.end(), temp.begin(), temp.end());
-        return ans;
-        
+            
+        for(int j = i; j < nums.size(); j++) {
+            //push the current element
+            temp.push_back(nums[j]);
+            //call for next
+            subset_helper(nums, ans, temp, j + 1, k);
+            //erase the data that was incorporated because of recursion call
+            temp.pop_back();
+        }
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-        return subsets_helper(nums, nums.size());
+        //final answer
+        vector<vector<int>> ans;
+        //store temp subset
+        vector<int> temp;
+        //running loop for length of subset
+        for(int k = 0; k <= nums.size(); k++) {
+            subset_helper(nums, ans, temp, 0, k);
+        }
+        return ans;
     }
 };
