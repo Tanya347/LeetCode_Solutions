dfc5f53bc2caaa11cab77bfe09f30c7c5d5de880
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-03-15 09:13:08 +0530

    Time: 7 ms (45.16%), Space: 11 MB (95.77%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
+    int search(vector<int>& nums, int target) {
     
-    int bSearch(vector<int> nums, int target, int s, int e) {
+        int s = 0, e = nums.size() - 1;
         
         while(s <= e) {
             
@@ -9,35 +10,28 @@ public:
             
             if(nums[mid] == target)
                 return mid;
-            else if(nums[mid] < target)
-                s = mid + 1;
-            else
-                e = mid - 1;
+            
+            else if(nums[mid] >= nums[s]) {
+                
+                //lower part of array is sorted
+                
+                if(target < nums[mid] && target >= nums[s])
+                    e = mid - 1;
+                else 
+                    s = mid + 1;
+            }
+            
+            else if(nums[mid] <= nums[e]) {
+                
+                //higher part of array is sorted
+                
+                if(target > nums[mid] && target <= nums[e])
+                    s = mid + 1;
+                else 
+                    e = mid - 1;
+            }
         }
         
         return -1;
-    }
-    
-    int search(vector<int>& nums, int target) {
-        
-        int pivot = -1;
-        
-        for(int i = 0; i < nums.size() - 1; i++) {
-            if(nums[i] > nums[i + 1])
-                pivot = i;
-        }
-        
-        if(pivot == -1) {
-            return bSearch(nums, target, 0, nums.size() - 1);
-        }
-        else {
-            int ans1 = bSearch(nums, target, 0, pivot);
-            
-            if(ans1 == -1) {
-                int ans2 = bSearch(nums, target, pivot + 1, nums.size() - 1);
-                return ans2;
-            }
-            else return ans1;
-        }
     }
 };
