68d498e22d4016ccaad834cc2f71e8c81cbf8404
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-03-19 08:48:28 +0530

    Time: 28 ms (51.79%), Space: 30.8 MB (5.57%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -2,20 +2,6 @@ class Solution {
 public:
     int directions[4][2] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     
-    void findGridArea(vector<vector<int>>& grid, int& area, int i, int j, int m, int n, vector<vector<bool>>& visited) {
-        
-        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1 || visited[i][j])
-            return;
-        
-        area++;
-        visited[i][j] = true;
-        
-        for(int k = 0; k < 4; k++) {
-            findGridArea(grid, area, i + directions[k][0], j + directions[k][1], m, n, visited);
-        }
-        
-    }
-    
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         
         int m = grid.size();
@@ -31,9 +17,35 @@ public:
                 if(grid[i][j] == 1 && !visited[i][j]) {
                      
                     int curr_area = 0;
-                    findGridArea(grid, curr_area, i, j, m, n, visited);
-                    if(curr_area > max_area)
-                        max_area = curr_area;
+                    
+                    stack<int> str;
+                    stack<int> stc;
+                    
+                    str.push(i);
+                    stc.push(j);
+                    
+                    visited[i][j] = true;
+                    
+                    while(!str.empty()) {
+                        
+                        int r = str.top(), c = stc.top();
+                        str.pop();
+                        stc.pop();
+                        
+                        curr_area++;
+                        
+                        for(int k = 0; k < 4; k++) {
+                            int nr = r + directions[k][0];
+                            int nc = c + directions[k][1];
+                            if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1 && !visited[nr][nc]) {
+                                str.push(nr);
+                                stc.push(nc);
+                                
+                                visited[nr][nc] = true;
+                            }
+                        }
+                    }
+                    max_area = max(max_area, curr_area);
                 }
             }
         }
